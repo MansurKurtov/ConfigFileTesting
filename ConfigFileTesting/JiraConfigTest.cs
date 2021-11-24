9f65196a849b5866c72795922f517aca10ef5945
Mansur <kurtov.mansur@gmail.com>
2021-11-24 10:55:55 +0500

    Add project files.

---
+++ ConfigFileTesting/JiraConfigTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace ConfigFileTesting
+{
+    public class JiraConfigTest
+    {
+        public string GetConfigData()
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("genesisjiraconfig.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            var jiraUrl = config.GetSection("Jira")["Url"];
+            var basicToken = config.GetSection("Jira")["Token"];
+            var tokenLifeTime = config.GetSection("Audience")["ExpireMinutes_access_token"];
+            
+            return jiraUrl;
+        }
+    }
+}
